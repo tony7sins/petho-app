be47dfd20362cb3fbb480d3e42e68f9d8d426744
tony7sins <tony7sins@gmail.com>
2020-06-25 15:15:51 +0300

    ⭐ Feat: Added PetSizeCalculator Service

---
+++ src/Service/PetSizeCalculator.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Service;
+
+use InvalidArgumentException;
+
+class PetSizeCalculator
+{
+    /**
+     * Simple Universal Pet Size Calculator
+     *
+     * @param array $sizes
+     * @param integer $height
+     * @param integer $weight
+     * @return string
+     * @throws \InvalidArgumentException
+     */
+    public function calculateSize(array $sizes, int $height, int $weight): string
+    {
+        if ($height <= 0 || $weight <= 0) {
+            throw new \InvalidArgumentException("arguments should not be <= 0", 100);
+        }
+
+        foreach ($sizes as $size => $measures) {
+            if (
+                $height <= $measures['height'] &&
+                $weight <= $measures['weight']) {
+
+                return $size;
+            }
+        }
+        return 'huge';
+    }
+}
---
+++ src/Structures/PetSize.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Structures;
+
+class PetSize
+{
+    const DOG = [
+        'tiny' => [
+            'height' => 300,
+            'weight' => 5000,
+        ],
+        'small' => [
+            'height' => 400,
+            'weight' => 10000,
+        ],
+        'middle' => [
+            'height' => 560,
+            'weight' => 20000,
+        ],
+        'big' => [
+            'height' => 600,
+            'weight' => 30000,
+        ],
+        'huge' => [
+            'height' => 650,
+            'weight' => 50000,
+        ],
+    ];
+}
