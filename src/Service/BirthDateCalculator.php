ede75a5423caaeee9511656d66befba2d819273e
tony7sins <tony7sins@gmail.com>
2020-06-25 19:53:30 +0300

    ⭐ Feat: Added BirthDateCalculator Service

---
+++ src/Service/BirthDateCalculator.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Service;
+
+class BirthDateCalculator
+{
+    /** @var \DateTime */
+    private $currentDate;
+
+    public function __construct()
+    {
+        $this->currentTime = $this->setCurrentDate();
+    }
+
+    public function calculateBirthDate(int $monthsAge): string
+    {
+        $date = new \DateTime("-{$monthsAge} months");
+
+        return $date->format('Y-m');
+    }
+
+    public function calculateAge(string $date): int
+    {
+        $pastDate = new \DateTime($date);
+        $dateDiff = $this->currentDate->diff($pastDate);
+        $different = (($dateDiff->y) * 12 + ($dateDiff->m));
+        return $different;
+    }
+
+    public function setCurrentDate(string $time = 'now'): void
+    {
+        $this->currentDate = new \DateTime($time);
+    }
+}
