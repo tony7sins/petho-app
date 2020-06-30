1231ded1c2205d9453c45af4c9befc7cafa24068
tony7sins <tony7sins@gmail.com>
2020-06-30 18:19:28 +0300

    🧩 Service: Added UuidGenerator

---
+++ src/Utils/UuidGenerator.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Utils;
+
+use Ramsey\Uuid\Uuid;
+use Webmozart\Assert\Assert;
+
+class uuidGenerator
+{
+    private $value;
+
+    public function __construct(string $value)
+    {
+        Assert::notEmpty($value);
+        $this->value = $value;
+    }
+
+    public static function next(): self
+    {
+        return new self(Uuid::uuid4()->toString());
+    }
+
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    public function __toString(): string
+    {
+        return $this->value;
+    }
+}
