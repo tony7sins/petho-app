8852a962b7198f026babe24742745b9a828f0c35
tony7sins <tony7sins@gmail.com>
2020-06-30 18:21:12 +0300

    ✔️ Test: Passed CreateOwnerTest

--- tests/UseCase/CreateOwnerTest.php
+++ tests/UseCase/CreateOwnerTest.php
@@ -1,59 +1,66 @@
 <?php
 namespace Tests\UseCase;
 
-use App\UseCase\CreateDog\Command\AddNewDog;
+use App\Entity\User;
+use App\UseCase\Command\CreateOwner;
+use App\UseCase\Factory\OwnerCreator;
+use App\Utils\UuidGenerator;
 use PHPUnit\Framework\TestCase;
 
 class CreateOwnerTest extends TestCase
 {
-    private $dogOne;
+    // private $dogOne;
 
-    public function setUp()
-    {
-        $name = 'Trump';
-        $height = 560;
-        $weight = 20000;
-        $age = 13;
+    // public function setUp()
+    // {
+    //     $name = 'Trump';
+    //     $height = 560;
+    //     $weight = 20000;
+    //     $age = 13;
 
-        $isActive = true;
-        $isMale = true;
-        $isStelized = true;
-        $isCaptived = false;
+    //     $isActive = true;
+    //     $isMale = true;
+    //     $isStelized = true;
+    //     $isCaptived = false;
 
-        $newPet = new AddNewDog(
-            $isActive,
-            $isMale,
-            $isStelized,
-            $isCaptived,
-            $height,
-            $weight,
-            $age,
-            $name
-        );
+    //     $newPet = new AddNewDog(
+    //         $isActive,
+    //         $isMale,
+    //         $isStelized,
+    //         $isCaptived,
+    //         $height,
+    //         $weight,
+    //         $age,
+    //         $name
+    //     );
 
-        /** @var \App\Entity\Dog $dog */
-        $dog = $this->creator->create($newPet);
+    //     /** @var \App\Entity\Dog $dog */
+    //     $dog = $this->creator->create($newPet);
 
-        $this->dogOne = $dog;
-    }
+    //     $this->dogOne = $dog;
+    // }
 
     public function test_create_new_owner()
     {
         $userName = 'John Malkovich';
         $userEmail = 'john@malkovich@test';
 
-        $userDTO = new CreateOwner(
-            $userName, $userEmail, $password = null
+        $userDto = new CreateOwner(
+            $userName, $userEmail, $password = ''
         );
 
-        $ownerFactory = new OwnerCreator();
+        $ownerFactory = new OwnerCreator(UuidGenerator::next());
 
         /** @var User */
         $owner = $ownerFactory
-            ->create()
-            ->addPet($this->dogOne)
+            ->create($userDto)
+            // ->addPet($this->dogOne)
         ;
 
         $this->assertInstanceOf(User::class, $owner);
+        $this->assertSame($userName, $owner->getName());
+        $this->assertSame($userEmail, $owner->getEmail());
+
+        dump($owner);
     }
 }
